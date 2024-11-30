d4f1770cb16f2216dd256b4f261a5a7f811ab4a7
Annapurna <jadhavannapurna37@gmail.com>
2024-11-30 10:47:40 +0530

    1st question on variable and operator

---
+++ varope.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+// 1. find the sum of two numbers
+
+int x, y;
+cout<<"enter your number x ";
+cin>>x;
+cout<<"enter your number y ";
+cin>>y;
+cout<<"sum of your numbers is "<<(x+y)<<"\n";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
+
