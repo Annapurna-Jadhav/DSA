ff059d0085ff45c615077769409d4e5e65843010
Annapurna <jadhavannapurna37@gmail.com>
2024-11-30 19:23:28 +0530

    day2 file

---
+++ DSA/CP/day2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // conditional statements
+
+    // if else
+    int n;
+    cout << "enter your number: ";
+    cin >> n;
+    if (n > 0){
+        cout<<"positive number ";
+    } 
+    else
+    {
+        cout << "negative number";
+    }
+    return 0
+}
