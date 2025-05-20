585e195d14cc6f0f9b62b5e17a02d758267285d1
Annapurna <jadhavannapurna37@gmail.com>
2025-05-20 11:31:55 +0530

    solved the small bug

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 
 void bSort(int arr[],int n){
-    for (int i=0; i<=n-i;i++){
+    for (int i=0; i<=n-1;i++){
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 int temporary=arr[j];
